51216782b1dae4aedb1f9ca700fa5d7448c77be7
Graham Land <graham@allthingscloud.eu>
2018-09-05 21:31:43 +0100

    removed hardcoding in tests too

--- scripts/test_vault_dynamic_creds.sh
+++ scripts/test_vault_dynamic_creds.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
-set -x
 source /usr/local/bootstrap/var.env
 
+echo 'Start Vault MongoDB Dynamic Credentials Testing'
 
 IP=${LEADER_IP}
-
+DB=${MONGO_IP}
 if [ "${TRAVIS}" == "true" ]; then
-   IP="127.0.0.1"
+    IP="127.0.0.1"
+    DB=${IP}
 fi
 
 VAULT_ADDR=http://${IP}:8200
@@ -17,8 +18,8 @@ VAULT_TOKEN=`cat /usr/local/bootstrap/.vault-token`
 test_db_user () {
     DYNAMIC_USER=`cat /usr/local/bootstrap/.dynamicuserdetails.txt | awk '{ for (x=1;x<=NF;x++) if ($x~"username") print $(x+1) }'`
     DYNAMIC_PASSWORD=`cat /usr/local/bootstrap/.dynamicuserdetails.txt | awk '{ for (x=1;x<=NF;x++) if ($x~"password") print $(x+1) }'`
-    mongo 192.168.2.12/vault_demo_db -u ${DYNAMIC_USER} -p ${DYNAMIC_PASSWORD} --eval "printjson(db.getUsers())"
-    mongo 192.168.2.12/vault_demo_db -u ${DYNAMIC_USER} -p ${DYNAMIC_PASSWORD} --eval "printjson(db.getName())"
+    mongo ${DB}/vault_demo_db -u ${DYNAMIC_USER} -p ${DYNAMIC_PASSWORD} --eval "printjson(db.getUsers())"
+    mongo ${DB}/vault_demo_db -u ${DYNAMIC_USER} -p ${DYNAMIC_PASSWORD} --eval "printjson(db.getName())"
 }
 
 sudo VAULT_TOKEN=${VAULT_TOKEN} VAULT_ADDR="http://${IP}:8200" vault status
@@ -30,3 +31,5 @@ test_db_user
 echo "Testing the DB Owner Role - This should successfully list all the users"
 sudo VAULT_TOKEN=${VAULT_TOKEN} VAULT_ADDR="http://${IP}:8200" vault read database/creds/my-dbOwner-role > /usr/local/bootstrap/.dynamicuserdetails.txt
 test_db_user
+
+echo 'Finished Vault MongoDB Dynamic Credentials Testing'
