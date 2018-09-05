f1f8ce3e4482d7ac37c165a4dec45e6581b0b4eb
Graham Land <graham@allthingscloud.eu>
2018-09-05 20:25:52 +0100

    travis debug2

--- scripts/test_vault_dynamic_creds.sh
+++ scripts/test_vault_dynamic_creds.sh
@@ -10,9 +10,9 @@ if [ "${TRAVIS}" == "true" ]; then
 IP=${IP:-127.0.0.1}
 fi
 
-export VAULT_ADDR=http://${IP}:8200
-export VAULT_SKIP_VERIFY=true
-export VAULT_TOKEN=`cat /usr/local/bootstrap/.vault-token`
+VAULT_ADDR=http://${IP}:8200
+VAULT_SKIP_VERIFY=true
+VAULT_TOKEN=`cat /usr/local/bootstrap/.vault-token`
 
 test_db_user () {
     DYNAMIC_USER=`cat /usr/local/bootstrap/.dynamicuserdetails.txt | awk '{ for (x=1;x<=NF;x++) if ($x~"username") print $(x+1) }'`
@@ -22,9 +22,9 @@ test_db_user () {
 }
 
 echo "Testing the DB Admin Role - This should fail to list all the users"
-vault read database/creds/my-dbAdmin-role > /usr/local/bootstrap/.dynamicuserdetails.txt
+sudo VAULT_TOKEN=${VAULT_TOKEN} VAULT_ADDR="http://${IP}:8200" vault read database/creds/my-dbAdmin-role > /usr/local/bootstrap/.dynamicuserdetails.txt
 test_db_user
 
 echo "Testing the DB Owner Role - This should successfully list all the users"
-vault read database/creds/my-dbOwner-role > /usr/local/bootstrap/.dynamicuserdetails.txt
+sudo VAULT_TOKEN=${VAULT_TOKEN} VAULT_ADDR="http://${IP}:8200" vault read database/creds/my-dbOwner-role > /usr/local/bootstrap/.dynamicuserdetails.txt
 test_db_user
