1597cb1114c6ba7934fb177ce455926659ddb20e
Graham Land <graham@allthingscloud.eu>
2018-09-05 14:18:51 +0100

    update mongo script

--- scripts/install_mongodb.sh
+++ scripts/install_mongodb.sh
@@ -27,19 +27,19 @@ setup_environment () {
 install_mongodb () {
 
     # Install MongoDB
-    apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
-    echo "deb [ arch=amd64 ] http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-3.4.list
-    apt-get update
-    apt-get install -y mongodb-org
-    service mongod status
+    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+    sudo echo "deb [ arch=amd64 ] http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-3.4.list
+    sudo apt-get update
+    sudo apt-get install -y mongodb-org
+    sudo service mongod status
     sleep 60
     # Add new database user accounts 
-    mongo conf/configureMongoDBusers.js
+    sudo mongo conf/configureMongoDBusers.js
     # Enable MongoDB Authentication
-    echo "security:\n  authorization:\tenabled" >> /etc/mongod.conf
+    sudo echo "security:\n  authorization:\tenabled" >> /etc/mongod.conf
     # Bind to all interfaces - not just localhost
-    sed -i '/bindIp:/s/^/#/' /etc/mongod.conf
-    service mongod restart
+    sudo sed -i '/bindIp:/s/^/#/' /etc/mongod.conf
+    sudo service mongod restart
 
 
 }
